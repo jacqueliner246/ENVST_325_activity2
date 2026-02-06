eaa9ceba55ca3e879175ae55a429c7781b4d1218
Jacqueline Reynaga <jreynaga@hamilton.edu>
2026-02-06 23:28:50 +0000

    finished hw questions

--- activity2.R
+++ activity2.R
@@ -51,20 +51,82 @@ max.cat <- floods %>%
   summarize(n.major = n())
 
 # earliest date that each river reached flood stage
-early.flood <- floods %>% 
-  group_by(names) %>% 
+min.flood.date <- floods %>% 
   filter(gheight.ft >= flood.ft) %>% 
+  group_by(names) %>%
   summarise(earliest.flood.date = min(dateF))
 
 # homework prompts
 # prompt 1
-# get stream stage data for each stream
-peaceRiver <- floods %>% 
-  filter(siteID == 2256500)
-fisheatingCreek <- floods %>% 
+# plot stream stage data for each stream
+peaceRiver <- streamH %>% 
   filter(siteID == 2295637)
-santaFeRiver <- floods %>% 
-  filter(siteID == 2322500)
-withlacoocheeRiver <- floods %>% 
-  filter(siteID == 2312000)
+plot(peaceRiver$dateF, peaceRiver$gheight.ft, 
+     type = "b", 
+     pch = 19, 
+     xlab = "Date", 
+     ylab = "Stage Height of Peace River (ft)")
 
+fisheatingCreek <- streamH %>% 
+  filter(siteID == 2256500)
+plot(fisheatingCreek$dateF, fisheatingCreek$gheight.ft, 
+     type = "b", 
+     pch = 19, 
+     xlab = "Date", 
+     ylab = "Stage Height of Fisheating Creek (ft)")
+
+santaFeRiver <- streamH %>% 
+  filter(siteID == 2322500)
+plot(santaFeRiver$dateF, santaFeRiver$gheight.ft, 
+     type = "b", 
+     pch = 19, 
+     xlab = "Date", 
+     ylab = "Stage Height of Santa Fe River (ft)")
+
+withlacoocheeRiver <- streamH %>% 
+  filter(siteID == 2312000)
+plot(withlacoocheeRiver$dateF, withlacoocheeRiver$gheight.ft, 
+     type = "b", 
+     pch = 19, 
+     xlab = "Date", 
+     ylab = "Stage Height of Withlacoochee River (ft)")
+
+
+# prompt 2
+# earliest date of occurance for each flood category for each river
+min.action.date <- floods %>% 
+  group_by(names) %>%
+  filter(gheight.ft >= action.ft) %>% 
+  summarise(earliest.action.date = min(dateF))
+
+min.moderate.date <- floods %>%
+  group_by(names) %>%
+  filter(gheight.ft >= moderate.ft) %>% 
+  summarise(earliest.moderate.date = min(dateF))
+
+min.major.date <- floods %>%
+  group_by(names) %>%
+  filter(gheight.ft >= major.ft) %>% 
+  summarise(earliest.major.date = min(dateF))
+
+full.flood.cat <- left_join(min.action.date, min.flood.date, by = "names")
+full.flood.cat <- left_join(full.flood.cat, min.moderate.date, by = "names")
+full.flood.cat <- left_join(full.flood.cat, min.major.date, by = "names")
+  
+# prompt 3
+# river that has the highest stage height above major flood category
+max.above.major <- floods %>%  
+  group_by(names, major.ft) %>% 
+  summarize(max.height = max(gheight.ft))
+max.above.major$height.above.major = max.above.major$max.height - max.above.major$major.ft
+
+# prompt 4
+# ifelse and hist r documentation and examples
+?ifelse()
+flooded <- floods %>% 
+  group_by(names) %>% 
+  mutate(flooded = ifelse(gheight.ft > major.ft, TRUE, FALSE)) %>% 
+  ungroup()
+
+?hist()
+hist(peaceRiver$gheight.ft, xlab = "Stage Height (ft)", main = "Histogram of Peace River Stage Heights")
